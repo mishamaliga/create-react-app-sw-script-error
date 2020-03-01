c4765b3ba30aca0e3522736077a47e81050c8ca5
Misha Maliga <mihmaliga@gmail.com>
2020-03-01 20:15:32 +0300

    Add the code to reproduce the 'Script error.' problem

--- src/index.js
+++ src/index.js
@@ -4,9 +4,19 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+window.addEventListener('error', (event) => {
+  console.log('error event: ', event)
+})
+
+const queryParams = new URLSearchParams(window.location.search)
+const shouldThrow = queryParams.get('shouldThrow')
+if (shouldThrow) {
+  throw new Error('Test error')
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
